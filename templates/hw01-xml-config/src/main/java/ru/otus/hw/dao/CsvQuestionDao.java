860f558f7d39e8f00c176c07b4fd65727fea9fd5
Kalmar73 <191101eEE>
2025-04-29 05:25:31 +0300

    Added more questions, completed functionality for basic tests

--- templates/hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ templates/hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,9 +1,13 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +21,16 @@ public class CsvQuestionDao implements QuestionDao {
         // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        List<Question> questions = new ArrayList<>();
+        try{
+            List<QuestionDto> questionDto = new CsvToBeanBuilder<QuestionDto>(new FileReader(fileNameProvider.getTestFileName())).withSeparator(';').withType(QuestionDto.class).withSkipLines(1).build().parse();
+            for (QuestionDto question:
+                 questionDto) {
+                questions.add(question.toDomainObject());
+            }
+        } catch (Exception e){
+            throw new QuestionReadException("Cant load questions!");
+        }
+        return questions;
     }
 }
